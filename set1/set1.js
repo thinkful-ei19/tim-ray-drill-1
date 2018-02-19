ff09afa7a59abe1b46c91e95f61443266eee5b68
Timothy Song <tsong0220@gmail.com>
2018-02-19 14:07:33 -0700

    add try/catch

--- set1/set1.js
+++ set1/set1.js
@@ -3,12 +3,26 @@ function yearOfBirth(age){
 }
 
 function whoAmI(name, age){
+    try {
+        if(age < 0) throw new Error("Age can not be negative");
+        if(name && age === undefined) console.error("Arguments not valid");
+        if(typeof age !== "number") console.error("Arguments not valid");
+        if(typeof name !== "string")  console.error("Arguments not valid");
+      }
+      
+      catch (e) {
+        console.log("fail");
+        console.dir(e);
+        
+      }
 var yob = yearOfBirth(age);
  console.log(`Hi my name is ${name} and I'm ${age} years old`);
 
-console.log("I was born in " + yob);
+ console.log("I was born in " + yob);
   
-};
-whoAmI("Chris", 29);
 
+
+
+}
+whoAmI(29, -29);
 
