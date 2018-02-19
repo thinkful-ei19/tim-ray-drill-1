8b39dc4ea60bdca031a2d2b68d86900a95e5de98
Timothy Song <tsong0220@gmail.com>
2018-02-19 12:54:49 -0700

    added parameters

--- set1/set1.js
+++ set1/set1.js
@@ -1,4 +1,6 @@
-function whoAmI{
-console.log("Hi my name is Chris and I'm 29 years old");
+function whoAmI(name, age){
+console.log(`Hi my ${name} is Chris and I'm ${age} years old`);
 };
-whoAmI();
+whoAmI("Chris", 29);
+
+
